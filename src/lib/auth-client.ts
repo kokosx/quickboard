71a887ffa00c30b9e9244e04ca1fd960c051ca7a
kokosx <agabart11@gmail.com>
2025-03-17 17:46:13 +0100

    feat: added basic setup and libraries

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,5 @@
+import { createAuthClient } from "better-auth/react";
+import { env } from "../env";
+export const authClient = createAuthClient({
+  baseURL: env.NEXT_PUBLIC_APP_URL, // the base url of your auth server
+});
